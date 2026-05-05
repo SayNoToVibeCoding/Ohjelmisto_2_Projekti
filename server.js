d3f3d449e64ee2bb326954788f259369748bd988
ronik <ronikais@metropolia.fi>
2026-05-05 16:37:41 +0300

    Updated server.js once again

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,)));
 
 const db = mysql.createConnection({
     host: "localhost",
