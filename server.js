03c0dd89271145a311c1fbc226f3cf402e1cf0f0
SayNoToVibeCoding <ronikais@metropolia.fi>
2026-05-05 10:07:37 +0300

    Itse peli HTML lisätty, updatettu Ahmedin login.html ja server.js jotta se toimii omien sivujen kanssa, style css lisätty

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "ahmed12345",
-    database: "game"
+    password: "roni1234",
+    database: "testi"
 });
 
 db.connect(err => {
@@ -20,14 +20,26 @@ db.connect(err => {
 });
 
 app.post("/login", (req, res) => {
-    const { user, password } = req.body;
+    const user = req.body.user;
+    const password = req.body.password;
 
-    const sql = "INSERT INTO users (user, password) VALUES (?, ?)";
+    const checkSql = "SELECT * FROM users WHERE user = ? AND password = ?";
+    
 
-    db.query(sql, [user, password], (err, result) => {
-        if (err) throw err;
-        res.send("Saved");
+    db.query(checkSql, [user, password], (err, result) => {
+        if (err) {
+            res.status(500).json({ status: "error", message: "Tietokantavirhe" });
+            return;
+        }
+ 
+        if (result.length > 0) {
+            res.json({ status: "ok", message: "Kirjautuminen onnistui" });
+        } 
+        else {
+            res.status(401).json({ status: "error", message: "Väärä käyttäjänimi tai salasana" });
+        }
     });
 });
+
 
 app.listen(3000, () => console.log("Server running on port 3000"));
